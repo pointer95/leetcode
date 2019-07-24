b9a615858a6fa22098d4725b2f9a0575aa7a5305
pointer <pointer2077@gmail.com>
2019-07-24 21:41:19 +0800

    first commit

---
+++ concurrency/lc1115_print_foobar_alternately/FooBar.java
@@ -0,0 +1,32 @@
+package concurrency.lc1115_print_foobar_alternately;
+
+class FooBar {
+    private int n;
+    private volatile boolean isFoo = true;
+
+    public FooBar(int n) {
+        this.n = n;
+    }
+
+    public void foo(Runnable printFoo) throws InterruptedException {
+
+        for (int i = 0; i < n; i++) {
+            while (!isFoo) {
+            }
+            // printFoo.run() outputs "foo". Do not change or remove this line.
+            printFoo.run();
+            isFoo = false;
+        }
+    }
+
+    public void bar(Runnable printBar) throws InterruptedException {
+
+        for (int i = 0; i < n; i++) {
+            while(isFoo) {
+            }
+            // printBar.run() outputs "bar". Do not change or remove this line.
+            printBar.run();
+            isFoo = true;
+        }
+    }
+}
