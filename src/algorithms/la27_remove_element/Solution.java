d13f6424c95631357a48fbe68572b0d4d62a9c8a
pointer <pointer2077@gmail.com>
2019-08-01 22:42:52 +0800

    add solution for 26, 27

---
+++ src/algorithms/la27_remove_element/Solution.java
@@ -0,0 +1,19 @@
+package algorithms.la27_remove_element;
+
+/**
+ * remove element
+ * source: https://leetcode-cn.com/problems/remove-element/
+ * date: 2019-08-01
+ */
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int i = 0;
+        for (int j = 0; j < nums.length; j ++) {
+            if (nums[j] != val) {
+                nums[i] = nums[j];
+                i ++;
+            }
+        }
+        return i;
+    }
+}
