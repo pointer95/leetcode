8024f93dc3a92dccbecec545d0300c7d62010145
pointer <pointer2077@gmail.com>
2019-08-11 16:57:48 +0800

    add solution for recursion: 70

---
+++ algorithms/src/la70_climbing_stairs/Solution.java
@@ -0,0 +1,34 @@
+package la70_climbing_stairs;
+
+import java.util.HashMap;
+
+/**
+ * source: https://leetcode-cn.com/problems/climbing-stairs/
+ * date: 2019-08-11
+ */
+class Solution {
+
+    private HashMap<Integer, Integer> map;
+
+    public Solution() {
+        map = new HashMap<>();
+    }
+
+    public int climbStairs(int n) {
+        return climb(n);
+    }
+
+    private int climb(int n) {
+        if (n == 1) {
+            return 1;
+        }
+        if (n == 2) {
+            return 2;
+        }
+        if (map.containsKey(n)) {
+            return map.get(n);
+        }
+        map.put(n, climb(n-1) + climb(n-2));
+        return map.get(n);
+    }
+}
