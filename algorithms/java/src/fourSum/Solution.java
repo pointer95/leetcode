8f389734c0ecf310641c1ce6640d90d27c863d3b
pointer2077 <pointer2077@gmail.com>
2019-09-29 14:31:03 +0800

    update for 18

--- algorithms/java/src/fourSum/Solution.java
+++ algorithms/java/src/fourSum/Solution.java
@@ -11,12 +11,17 @@ import java.util.List;
 class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
         Arrays.sort(nums);
+        int len = nums.length;
         List<List<Integer>> res = new ArrayList<>();
-        for (int i = 0; i < nums.length - 3; ++i) {
+        for (int i = 0; i < len - 3; ++i) {
             if (i > 0 && nums[i] == nums[i-1]) continue;
-            for (int j = i + 1; j < nums.length - 2; ++j) {
+            if (nums[i] + nums[i+1] + nums[i+2] + nums[i+3] > target) break;
+            if (nums[i] + nums[len-1] + nums[len-2] + nums[len-3] < target) continue;
+            for (int j = i + 1; j < len - 2; ++j) {
                 if (j > i + 1 && nums[j] == nums[j-1]) continue;
-                int first = j + 1, last = nums.length - 1;
+                if (nums[i] + nums[j] + nums[j+1] + nums[j+1] > target) break;
+                if (nums[i] + nums[j] + nums[len-1] + nums[len-2] < target) continue;
+                int first = j + 1, last = len - 1;
                 while (first < last) {
                     int result = nums[i] + nums[j]+ nums[first] + nums[last];
                     if (result == target) {
