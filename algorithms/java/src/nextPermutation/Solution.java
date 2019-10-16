c2788d380d2d46a76932fe8228b73de58d12def3
pointer <pointer2077@gmail.com>
2019-10-16 21:31:03 +0800

    add solution for 31

---
+++ algorithms/java/src/nextPermutation/Solution.java
@@ -0,0 +1,39 @@
+package nextPermutation;
+
+import java.util.Arrays;
+
+/**
+ * source: https://leetcode-cn.com/problems/next-permutation/
+ * date: 2019/10/16
+ */
+class Solution {
+    public void nextPermutation(int[] nums) {
+        int i = nums.length - 2;
+        while (i >= 0 && nums[i + 1] <= nums[i]) {
+            i--;
+        }
+        if (i >= 0) {
+            int j = nums.length - 1;
+            while (j >= 0 && nums[j] <= nums[i]) {
+                j--;
+            }
+            swap(nums, i, j);
+        }
+        reverse(nums, i + 1);
+    }
+
+    private void reverse(int[] nums, int start) {
+        int i = start, j = nums.length - 1;
+        while (i < j) {
+            swap(nums, i, j);
+            i++;
+            j--;
+        }
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
