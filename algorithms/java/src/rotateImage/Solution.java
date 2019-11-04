bac4d958f9646754e5df145859798c4767a466b4
pointer2077 <pointer2077@gmail.com>
2019-11-04 15:43:50 +0800

    add solution for 48

---
+++ algorithms/java/src/rotateImage/Solution.java
@@ -0,0 +1,20 @@
+package rotateImage;
+
+/**
+ * source: https://leetcode-cn.com/problems/rotate-image/
+ * date: 2019/11/4
+ */
+class Solution {
+    public void rotate(int[][] matrix) {
+        int n = matrix.length;
+        for (int i = 0; i < n/2; i++) {
+            for (int j = i; j < n-i-1; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[n-j-1][i];
+                matrix[n-j-1][i] = matrix[n-i-1][n-j-1];
+                matrix[n-i-1][n-j-1] = matrix[j][n-i-1];
+                matrix[j][n-i-1] = temp;
+            }
+        }
+    }
+}
