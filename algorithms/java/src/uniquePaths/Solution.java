c027f925b4d3c94aeecd880872b452eb2698d94c
pointer2077 <pointer2077@gmail.com>
2019-12-12 16:08:58 +0800

    add solution for 62

---
+++ algorithms/java/src/uniquePaths/Solution.java
@@ -0,0 +1,24 @@
+package uniquePaths;
+
+/**
+ * source: https://leetcode-cn.com/problems/unique-paths/
+ * @author: pointer
+ * @date: Created on 2019/12/12
+ */
+class Solution {
+    public int uniquePaths(int m, int n) {
+        int[][] dp = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            dp[i][0] = 1;
+        }
+        for (int j = 0; j < n; j++) {
+            dp[0][j] = 1;
+        }
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                dp[i][j] = dp[i-1][j] + dp[i][j-1];
+            }
+        }
+        return dp[m-1][n-1];
+    }
+}
