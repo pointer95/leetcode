d3a8c26faac8b76cbe607b39fd78e72ca733d1f6
pointer <pointer2077@gmail.com>
2019-12-29 22:13:11 +0800

    add solution for 88

---
+++ algorithms/java/src/mergeSortedArray/Solution.java
@@ -0,0 +1,18 @@
+package mergeSortedArray;
+
+/**
+ * source: https://leetcode-cn.com/problems/merge-sorted-array/
+ * @author: pointer
+ * @date: Created on 2019-12-29
+ */
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int len1 = m - 1;
+        int len2 = n - 1;
+        int len = m + n - 1;
+        while (len1 >= 0 && len2 >= 0) {
+            nums1[len--] = nums1[len1] > nums2[len2] ? nums1[len1--] : nums2[len2--];
+        }
+        System.arraycopy(nums2, 0, nums1, 0, len2+1);
+    }
+}
