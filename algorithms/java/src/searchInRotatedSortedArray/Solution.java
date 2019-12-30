f516156fed67325f0c66439a580905a1b624293d
pointer <pointer2077@gmail.com>
2019-12-30 16:53:21 +0800

    update for 33

--- algorithms/java/src/searchInRotatedSortedArray/Solution.java
+++ algorithms/java/src/searchInRotatedSortedArray/Solution.java
@@ -8,15 +8,17 @@ class Solution {
     public int search(int[] nums, int target) {
         if (nums == null || nums.length == 0) return -1;
         int low = 0, high = nums.length - 1;
-        while (low < high) {
-            int mid = (low + high) / 2;
+        int mid;
+        while (low <= high) {
+            mid = low + ((high - low) >> 1);
+            if (nums[mid] == target) return mid;
             if ((nums[0] <= nums[mid] && (target < nums[0] || target > nums[mid]))
                     || (nums[0] > nums[mid] && (target < nums[0] && target > nums[mid]))) {
                 low = mid + 1;
             } else {
-                high = mid;
+                high = mid - 1;
             }
         }
-        return low == high && nums[low] == target ? low : -1;
+        return -1;
     }
 }
