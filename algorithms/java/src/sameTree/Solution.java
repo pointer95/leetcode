fcc15d9593283d9d49241c35e6fd9fbf7545641c
pointer <pointer2077@gmail.com>
2020-01-04 23:18:27 +0800

    add solution for 100

---
+++ algorithms/java/src/sameTree/Solution.java
@@ -0,0 +1,18 @@
+package sameTree;
+
+import common.TreeNode;
+
+/**
+ * source: https://leetcode-cn.com/problems/same-tree/
+ * @author: pointer
+ * @date: Created on 2020-01-04
+ */
+class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null)return true;
+        if (p == null || q == null) return false;
+        if (p.val != q.val) return false;
+        return isSameTree(p.left, q.left)
+                && isSameTree(p.right, q.right);
+    }
+}
